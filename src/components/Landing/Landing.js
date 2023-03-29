70a05bcb751d16c3d9b1f25e1521d6c64389777e
KyleRessLiere <kressliere@gmail.com>
2023-03-29 11:44:23 -0400

    landing page text added and placed accordingly

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,6 +3,11 @@ import React from "react";
 function Landing() {
   return (
     <div className="landing-container">
+      <div className="heading-wrapper">
+        <h1 className="heading-text">
+          The Fight for <br></br> Comprehensive <br></br> Sex Education
+        </h1>
+      </div>
       <img src="protest.png" alt="nav-ico" className="landing-image" />
     </div>
   );
